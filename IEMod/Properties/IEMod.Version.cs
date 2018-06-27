9ff2c13e77472db519f0c5af11349c51c289b26c
BluesKutya <blueskutya@gmail.com>
2018-06-28 00:01:32 +0200

    update IE Mod version (based on ref. DLL changes)

--- IEMod/Properties/IEMod.Version.cs
+++ IEMod/Properties/IEMod.Version.cs
@@ -6,7 +6,7 @@ namespace IEMod {
 	/// </summary>
 	[NewType]
 	public static class IEModVersion {
-		public const string StupidVersion = "5.2.2";
-		public const string Version = "3.06.1 DLC";
+		public const string StupidVersion = "5.2.3";
+		public const string Version = "3.07.1 DLC";
 	}
 }
