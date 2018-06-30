c69bf7e0c7b7a574cf039d38376a404c67e0c5d9
BluesKutya <blueskutya@gmail.com>
2018-06-30 14:02:14 +0200

    Update reference files for Mod 5.2.4.0 Game version: 3.07.0 1318

--- IEMod/Properties/IEMod.Version.cs
+++ IEMod/Properties/IEMod.Version.cs
@@ -6,7 +6,7 @@ namespace IEMod {
 	/// </summary>
 	[NewType]
 	public static class IEModVersion {
-		public const string StupidVersion = "5.2.3.0";
-		public const string Version = "3.07.0 1280";
+		public const string StupidVersion = "5.2.4.0";
+		public const string Version = "3.07.0 1318";
 	}
 }
